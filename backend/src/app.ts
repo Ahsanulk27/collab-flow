6c1eae88e2a061d5e6f1e738b263216b6c9854d5
Ahsanul <ahsanulkarim2002@gmail.com>
2026-01-06 08:50:52 +0600

    deploy test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: `${process.env.FRONTEND_URL} || http://localhost:8080`,
+    origin: `${process.env.FRONTEND_URL}` || 'http://localhost:8080',
     credentials: true,
   })
 );
